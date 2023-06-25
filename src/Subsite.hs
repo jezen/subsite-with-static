4a13250c42147481740f648dc0800137cb13c5d2
Ari Fordsham <arifordsham@gmail.com>
2023-06-25 17:37:29 +0300

    Builds

--- src/Subsite.hs
+++ src/Subsite.hs
@@ -20,9 +20,6 @@ import StaticFiles
 import Subsite.Data
 import Yesod
 
-import Network.Wai
-import Yesod.Core.Types
-
 getSubHomeR :: Yesod master => SubHandlerFor Subsite master Html
 getSubHomeR = do
   toParent <- getRouteToParent
@@ -31,64 +28,4 @@ getSubHomeR = do
     [whamlet|Welcome to the subsite!|]
 
 instance Yesod master => YesodSubDispatch Subsite master where
-  -- yesodSubDispatch = $(mkYesodSubDispatch resourcesSubsite)
-  yesodSubDispatch =
-    let
-      helper_a3P0 =
-        inner_a3OZ
-       where
-        inner_a3OZ env6731_a3OO req6731_a3OP =
-          helper6731_a3OQ (pathInfo req6731_a3OP)
-         where
-          helper6731_a3OQ [] =
-            case requestMethod req6731_a3OP of
-              "GET" ->
-                (((subHelper getSubHomeR) env6731_a3OO) (Just SubHomeR))
-                  req6731_a3OP
-              _ ->
-                (((subHelper (void badMethod)) env6731_a3OO) (Just SubHomeR))
-                  req6731_a3OP
-          helper6731_a3OQ ((:) "static" restPath_a3OR) =
-            ( ( ( ( ( \_ getSub_a3OU toParent_a3OV env_a3OW ->
-                        yesodSubDispatch
-                          YesodSubRunnerEnv
-                            { ysreParentRunner = ysreParentRunner env_a3OW
-                            , ysreGetSub = (getSub_a3OU . ysreGetSub env_a3OW)
-                            , ysreToParentRoute = (ysreToParentRoute env_a3OW . toParent_a3OV)
-                            , ysreParentEnv = ysreParentEnv env_a3OW
-                            }
-                    )
-                      subHelper
-                  )
-                    (\sub_a3OX -> getStatic sub_a3OX)
-                )
-                  (\sroute_a3OY -> StaticR sroute_a3OY)
-              )
-                env6731_a3OO
-            )
-              ( ((\p_a3OS r_a3OT -> r_a3OT{pathInfo = p_a3OS}) restPath_a3OR)
-                  req6731_a3OP
-              )
-          helper6731_a3OQ _ =
-            (((subHelper (void notFound)) env6731_a3OO) Nothing) req6731_a3OP
-     in
-      helper_a3P0
-
-subHelper ::
-  ToTypedContent content =>
-  SubHandlerFor child master content ->
-  YesodSubRunnerEnv child master ->
-  Maybe (Route child) ->
-  Application
-subHelper (SubHandlerFor f) YesodSubRunnerEnv{..} mroute =
-  ysreParentRunner handler ysreParentEnv (fmap ysreToParentRoute mroute)
- where
-  handler = fmap toTypedContent $ HandlerFor $ \hd ->
-    let rhe = handlerEnv hd
-        rhe' =
-          rhe
-            { rheRoute = mroute
-            , rheChild = ysreGetSub $ yreSite ysreParentEnv
-            , rheRouteToMaster = ysreToParentRoute
-            }
-     in f hd{handlerEnv = rhe'}
+  yesodSubDispatch = $(mkYesodSubDispatch resourcesSubsite)

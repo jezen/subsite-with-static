07f442e1eb2d4bb3e71bc56f2cfc50c316a99824
AriFordsham <37623136+AriFordsham@users.noreply.github.com>
2023-06-27 21:12:28 +0300

    Ari/yesod change (#1)

--- src/Subsite.hs
+++ src/Subsite.hs
@@ -1,11 +1,12 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Subsite
   ( module Subsite.Data
@@ -13,12 +14,9 @@ module Subsite
   ) where
 
 import ClassyPrelude
-import Network.Wai qualified as W
-import Network.Wai.Internal (requestMethod)
 import StaticFiles
 import Subsite.Data
 import Yesod
-import Yesod.Core.Types
 
 getSubHomeR :: Yesod master => SubHandlerFor Subsite master Html
 getSubHomeR = do
@@ -28,38 +26,4 @@ getSubHomeR = do
     [whamlet|Welcome to the subsite!|]
 
 instance Yesod master => YesodSubDispatch Subsite master where
-  yesodSubDispatch env req =
-    case W.pathInfo req of
-      [] -> case requestMethod req of
-        "GET" -> subHelper getSubHomeR env (Just SubHomeR) req
-        _     -> subHelper (void badMethod) env (Just SubHomeR) req
-
-      ("static":rest) ->
-        yesodSubDispatch
-          YesodSubRunnerEnv
-            { ysreGetSub = getStatic . ysreGetSub env
-            , ysreToParentRoute = ysreToParentRoute env . StaticR
-            , ysreParentRunner = ysreParentRunner env
-            , ysreParentEnv = ysreParentEnv env
-            }
-          req { W.pathInfo = rest }
-
-      _ -> subHelper (void notFound) env Nothing req
-
-subHelper
-  :: ToTypedContent content
-  => SubHandlerFor child master content
-  -> YesodSubRunnerEnv child master
-  -> Maybe (Route child)
-  -> W.Application
-subHelper (SubHandlerFor f) YesodSubRunnerEnv {..} mroute =
-  ysreParentRunner handler ysreParentEnv (ysreToParentRoute <$> mroute)
-  where
-    handler = fmap toTypedContent $ HandlerFor $ \hd ->
-      let rhe = handlerEnv hd
-          rhe' = rhe
-            { rheRoute = mroute
-            , rheChild = ysreGetSub $ yreSite ysreParentEnv
-            , rheRouteToMaster = ysreToParentRoute
-            }
-       in f hd { handlerEnv = rhe' }
+  yesodSubDispatch = $(mkYesodSubDispatch resourcesSubsite)
